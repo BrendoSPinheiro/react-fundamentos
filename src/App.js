d1c85ef4833ac4ee0388b186c464edef5807a480
Brendo Souza Pinheiro <brendo.spinheiro@gmail.com>
2021-06-05 17:13:40 -0300

    feat: create post component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
 import React from 'react';
 
+import { Post } from './Post';
+
 export function App() {
-  return <h1>Teste</h1>;
+  return (
+    <>
+      <h1>Brendo's Blog</h1>
+      <h2>Posts da semana</h2>
+
+      <hr />
+
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+    </>
+  );
 }
---
+++ src/Post.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export function Post() {
+  return (
+    <>
+      <article>
+        <strong>Título do post</strong>
+        <br />
+        <small>Subtítulo do post</small>
+      </article>
+      <br />
+    </>
+  );
+}
