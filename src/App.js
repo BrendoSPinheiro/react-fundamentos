e13a754dfca52ab8e9ce1ab449c92ba7db5a4d22
Brendo Souza Pinheiro <brendo.spinheiro@gmail.com>
2021-06-10 22:40:35 -0300

    feat: use state for storage post list

--- src/App.js
+++ src/App.js
@@ -1,38 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Post } from './Post';
 import { Header } from './Header';
 
-const posts = [
-  {
-    title: 'Título da notícia 01',
-    subtitle: 'Subtítulo da notícia 01',
-    likes: 15,
-  },
-  {
-    title: 'Título da notícia 02',
-    subtitle: 'Subtítulo da notícia 02',
-    likes: 20,
-  },
-  {
-    title: 'Título da notícia 03',
-    subtitle: 'Subtítulo da notícia 03',
-    likes: 30,
-  },
-];
-
 export function App() {
+  const [posts, setPosts] = useState([
+    {
+      id: Math.random(),
+      title: 'Título da notícia 01',
+      subtitle: 'Subtítulo da notícia 01',
+      likes: 15,
+    },
+    {
+      id: Math.random(),
+      title: 'Título da notícia 02',
+      subtitle: 'Subtítulo da notícia 02',
+      likes: 20,
+    },
+    {
+      id: Math.random(),
+      title: 'Título da notícia 03',
+      subtitle: 'Subtítulo da notícia 03',
+      likes: 30,
+    },
+  ]);
+
+  function handleAddNewPost() {
+    setPosts((prevState) => [
+      ...prevState,
+      {
+        id: Math.random(),
+        title: `Título#0${prevState.length + 1}`,
+        subtitle: `Subtítulo#0${prevState.length + 1}`,
+        likes: 30,
+      },
+    ]);
+  }
+
   return (
     <>
       <Header title="Blog do Brendo">
-        <h2>Posts da semana</h2>
+        <h2>
+          Posts da semana
+          <button onClick={handleAddNewPost}>Novo Post</button>
+        </h2>
       </Header>
 
       <hr />
 
-      {posts.map((post, index) => (
+      {posts.map((post) => (
         <Post
-          key={index}
+          key={post.id}
           post={{
             title: post.title,
             subtitle: post.subtitle,
