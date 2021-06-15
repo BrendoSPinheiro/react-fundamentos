cd375e8b98bb479211618a96ce9327bdca5b3dc1
BrendoSPinheiro <brendo.spinheiro@gmail.com>
2021-06-15 06:18:27 -0300

    feat: use button component

--- src/PostHeader.js
+++ src/PostHeader.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import { Button } from './Button';
+
 export function PostHeader(props) {
   return (
     <>
       <strong>
         {props.post.read ? <s>{props.post.title}</s> : props.post.title}
       </strong>
-      <button onClick={() => props.onRemove(props.post.id)}>Remover</button>
+      <Button onClick={() => props.onRemove(props.post.id)}>Remover</Button>
     </>
   );
 }
